4dfe30f78e81d932d42221880a30007ed3979772
Brandon Harris <brandonharris177@gmail.com>
2021-04-18 15:03:21 -0600

    First QR code complete

---
+++ src/components/QRCode.vue
@@ -0,0 +1,18 @@
+<template>
+  <qrcode-vue :value="value" :size="size" level="H" />
+</template>
+<script>
+  import QrcodeVue from 'qrcode.vue'
+
+  export default {
+    data() {
+      return {
+        value: 'Brandon Harris',
+        size: 400,
+      }
+    },
+    components: {
+      QrcodeVue,
+    },
+  }
+</script>
