aab1fb855d4d47df712d8600a90de9203f66e6f0
Brandon Harris <brandonharris177@gmail.com>
2021-04-19 11:06:06 -0600

    feeds embedded

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
   <a href="https://www.linkedin.com/in/brandon-harris177/"> LinkedIn </a>
   <a href="mailto:brandonharris177@gmail.com"> brandonharris177@gmail.com </a>
   <a href="https://drive.google.com/file/d/1o_Qz3WAUTVgcdZk7os_WQ4p_IO_Gtc_L/view?usp=sharing"> Resume </a>
+  <a class="twitter-timeline" href="https://twitter.com/BBCAfrica?ref_src=twsrc%5Etfw">Tweets by BBCAfrica</a>
+  <a class="twitter-timeline" href="https://twitter.com/ethereum?ref_src=twsrc%5Etfw">Tweets by ethereum</a>
 </template>
 
 <script>
@@ -13,7 +15,12 @@ export default {
   name: 'App',
   components: {
     QRCodes,
-  }
+  },
+  mounted() {
+    let externalScript = document.createElement('script')
+    externalScript.setAttribute('src', 'https://platform.twitter.com/widgets.js')
+    document.head.appendChild(externalScript)
+  },
 }
 </script>
 
